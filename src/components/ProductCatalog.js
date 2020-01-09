d11a8710d8734511370e8aafba94ba0d00be3abb
Abdeali Daginawala <abdeali.daginawala@slalom.com>
2020-01-09 10:39:25 -0500

    added more catalog items and fixed image scaling

--- src/components/ProductCatalog.js
+++ src/components/ProductCatalog.js
@@ -6,17 +6,23 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from 'react-native';
 
-import { electronics } from '../services/DataService';
+import {electronics} from '../services/DataService';
 
 export default class ProductCatalog extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      catalog: []
+      catalog: [],
     };
   }
 
@@ -24,32 +30,68 @@ export default class ProductCatalog extends Component {
     this.setState({catalog: electronics});
   }
 
-  renderProducts = (products) => {
+  renderProducts = products => {
     return products.map((item, index) => {
       return (
         <View key={index} style={styles.item}>
           <TouchableOpacity onPress={() => {}} style={styles.addItem}>
-            <Text>{item.name + ' - $' + item.price}</Text>
+            <Text style={styles.itemText}>
+              {item.name + ' - $' + item.price}
+            </Text>
+            <Image
+              style={styles.itemImage}
+              source={{
+                uri: item.image,
+              }}
+            />
           </TouchableOpacity>
         </View>
-      )
-    })
-  }
+      );
+    });
+  };
 
   render() {
     return (
-      <View>
-        {this.renderProducts(this.state.catalog)}
-      </View>
+      <ScrollView style={styles.scrollContainer}>
+        <View style={styles.contentContainer}>
+          <View style={styles.itemContainer}>
+            {this.renderProducts(this.state.catalog)}
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  item: {
-    padding: 20
+  scrollContainer: {
+    flexGrow: 1,
   },
-  addItem: {
-
-  }
+  contentContainer: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    backgroundColor: '#FAEED4',
+  },
+  itemContainer: {
+    marginVertical: 20,
+    width: '80%',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  item: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: 'rgba(250, 128, 123, 0.5)',
+    width: '100%',
+  },
+  itemImage: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 1
+  },
+  itemText: {
+    fontSize: 18,
+  },
+  addItem: {},
 });
