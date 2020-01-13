a9242b49e19f4543adec6f690f6ebb8aa357825b
Abdeali Daginawala <abdeali.daginawala@slalom.com>
2020-01-13 13:20:36 -0500

    fixed bug with negative index

--- src/reducers/cartItems.js
+++ src/reducers/cartItems.js
@@ -6,10 +6,15 @@ const cartItems = (state = [], action) => {
       return [...state, action.payload];
     case REMOVE_FROM_CART:
       const itemIndexToRemove = state.findIndex(cartItem => cartItem.id === action.payload.id);
-      const resultArray = [...state.slice(0, itemIndexToRemove), ...state.slice(itemIndexToRemove + 1)];
-      return resultArray;
+      // this is bad practice, you should not have boolean logic in your reducer
+      if (itemIndexToRemove !== -1) {
+        const resultArray = [...state.slice(0, itemIndexToRemove), ...state.slice(itemIndexToRemove + 1)];
+        state = resultArray;
+      } 
+      return state;
+    default:
+      return state;
     }
-    return state;
 };
 
 
