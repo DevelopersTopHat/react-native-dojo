4b4fdcd15e3f784ba437afe730efd582cac97b48
Abdeali Daginawala <abdeali.daginawala@slalom.com>
2020-05-02 18:20:51 -0400

    added button component

--- src/components/ButtonComponent.js
+++ src/components/ButtonComponent.js
@@ -6,12 +6,24 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 // Fill out the return for exercise 1
 const ButtonComponent = props => {
   return (
-    <>
-  
-    </>
+    <View style={styles.buttonContainer}>
+      <Icon name="remove" size={30} color="white" onPress={() => {props.onPressAdd(props.item)}} style={styles.button} />
+      <Icon name="add" size={30} color="white" onPress={() => {props.onPressRemove(props.item)}} style={styles.button} />
+    </View>
   );
 };
 
 export default ButtonComponent;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  button: {
+    backgroundColor: "#005457",
+    borderRadius: 150
+  }
+});
